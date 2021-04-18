b9f498d21b1044bf441b91fac8a7852125e3ab69
Lucas Lopez <lucasaxellopez3@gmail.com>
2021-04-17 21:02:12 -0300

    primer commit

---
+++ src/pb2Git/Main.java
@@ -0,0 +1,15 @@
+package pb2Git;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Bienvenidos a la clase de PB2");
+		System.out.println("Clase de GIT");
+		int j = 9;
+		j++;
+		System.out.println(j);
+
+	}
+
+}
